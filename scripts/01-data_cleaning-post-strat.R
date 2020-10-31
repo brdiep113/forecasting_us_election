97231d95c405c909d2a77b5d5f95621a72bfade3
brdiep113 <brdiep113@gmail.com>
2020-10-30 23:49:27 -0400

    script changes

--- scripts/01-data_cleaning-post-strat.R
+++ scripts/01-data_cleaning-post-strat.R
@@ -1,12 +1,13 @@
 #### Preamble ####
 # Purpose: Prepare and clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander and Sam Caetano [CHANGE THIS TO YOUR NAME!!!!]
+# Author: Brian Diep
 # Data: 22 October 2020
 # Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
 # License: MIT
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
+# - Download the 2018 ACS dataset as detailed in the readme and save it to 
+#   inputs/data
+# - Don't share that data
 
 
 #### Workspace setup ####
@@ -25,22 +26,63 @@ names(raw_data)
 
 census_data <- 
   raw_data %>% 
-  select(region,
+  dplyr::select(
          stateicp,
          sex, 
          age, 
          race, 
-         hispan,
-         marst,
-         bpl,
-         citizen,
          educd,
+         hispan,
          labforce,
          inctot)
 rm(raw_data)
          
 
-#### What's next? ####
+#### Further Cleaning ####
 
+# Make age into groups
+census_data <- 
+  census_data %>%
+  mutate(age = case_when(
+    between(age, 0, 24) ~ "0-24",
+    between(age, 25, 34) ~ "25-34",
+    between(age, 35, 44) ~ "35-44",
+    between(age, 45, 54) ~ "45-54",
+    between(age, 55, 64) ~ "55-64",
+    between(age, 65, 74) ~ "65-74",
+    between(age, 75, Inf) ~ "75+"))
 
+# Change state column to match requirements
+census_data <-
+  census_data %>%
+  rename(state = stateicp)
+
+# Clean income
+census_data <-
+  census_data %>%
+  mutate(income = case_when(
+    between(inctot, -Inf, 20000) ~ "Under Poverty Line",
+    between(inctot, 20001, 44999) ~ "Low Income",
+    between(inctot, 45000, 99999) ~ "Lower Middle Income",
+    between(inctot, 100000, 149999) ~ "Upper Middle Income",
+    between(inctot, 150000, 199999) ~ "High Income",
+    between(inctot, 200000, Inf) ~ "Eat the rich"
+  ))
+
+# Clean race
+census_data <-
+  census_data %>%
+  mutate(race = case_when(
+    race == "black/african american/negro" ~ "black",
+    race == "chinese" | race == "japanese" | race == "other asian or pacific islander" ~ "asian or pacific islander",
+    race == "other race, nec" ~ "other",
+    race == "two major races" | race == "three or more major races" ~ "mixed",
+    race == "white" ~ "white",
+    race == "american indian or alaska native" ~ "native"
+  ))
+
+# Clean hispanic
+census_data <-
+  census_data %>%
+  mutate(hispan, ifelse(hispan == "not hispanic", "not hispanic", "hispanic"))
          
